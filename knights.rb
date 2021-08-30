130e49d7cb31f5c977f36b25c8eb6dc76d8a7a4c
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-30 13:28:45 -0400

    add valid moves

--- knights.rb
+++ knights.rb
@@ -9,33 +9,34 @@ require_relative 'polytree'
 # from starting position to an end position on 8 x 8 chess board
 
 class KnightPathFinder
-  def self.valid_moves(pos, visited = Set.new())
-
-    while possible.moves.length <= 8
-    # 8 possible moves
-      return nil if visited.include?(pos)
-
-      @considered_positions << pos
-      visited.add(pos)
+  POSITIONS = [[-2, 1],[-2, -1], [1, 2], [1, -2], [2, 1], [2, -1], [-1, -2], [-1, 2]]
+  def self.valid_moves(pos)
+      valid_moves = []
+    POSITIONS.each do |pos_move|
+      new_pos = [pos_move[0] + pos[0], pos_move[1] + pos[1]]
+      if (new_pos[0] >= 0 && new_pos[0] < 8) && (new_pos[1] >= 0 && new_pos[1] < 8)
+        valid_moves << new_pos
+      end
     end
-
+    valid_moves
   end
+
 
   attr_reader :starting_position
 
   def initialize(position)
-    @root_node = PolyTreeNode.new(position)
-    @considered_positions = [@root_node.value]
+    @starting_position = position
+    @considered_positions = [starting_position]
     self.build_move_tree
   end
 
   def path_find(position)
-
+    # search for the end pos recursively using dfs
 
   end
 
   def new_move_positions(new_pos)
-    KnightPathFinder.valid_moves()
+    KnightPathFinder.valid_moves(new_pos)
 
     return @considered_positions
   end
@@ -49,11 +50,11 @@ class KnightPathFinder
 
   def build_move_tree
     # start with self.root_node
-
+    @root_node = PolyTreeNode.new(position)
   end
 
 end
 
-# kpf = KnightPathFinder.new[0, 0]
+p KnightPathFinder.valid_moves([0, 0])
 # kpf.find_path([2, 1]) # => [[0, 0], [2, 1]]
 # kpf.find_path([3, 3]) # => [[0, 0], [2, 1], [3, 3]]
